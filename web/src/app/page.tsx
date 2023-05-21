8c168f16d1d6d0b3c5c6102a2b2be0f321cbc04c
Matheus Grossi <thebravematteo33@protonmail.com>
2023-05-20 21:57:36 -0300

    feat/space-time/adding blur, using tailwind. started adding stripes, using tailwind too (CSS)

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
-  return (
-    <div className="h-screen bg-zinc-900 text-zinc-50">
-      <h1 className="text-4xl"> Sua Capssssula do Tempo</h1>
+  return <main className="grid grid-cols-2 min-h-screen">
+    {/* Left */}
+    <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
+      {/* Blur */}
+      <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+      {/* Stripes */}
     </div>
-  )
+
+    {/* Right */}
+    <div className="flex flex-col p-16">
+      <div className="flex flex-1 items-center justify-center">
+        <p className="text-center leading-relaxed w-[360px] ">
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href="" className="underline hover:text-gray-50">criar agora!</a>!
+        </p>
+      </div>
+    </div>
+  </main >
+
+
 }
+
