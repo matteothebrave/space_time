49cc3a1a269295b6444cb3fce23e6893705136a8
Matheus Grossi <thebravematteo33@protonmail.com>
2023-06-18 20:53:58 -0300

    feat/space-time/added OAUTH and created .env.local for web project and added on server project (back-end)also created a client key and secret key for authentication

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes " />
 
       {/* Sign-In */}
-      <a href="" className="flex items-center gap-3 text-left transition-colors hover:text-gray-50 ">
+      <a href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`} className="flex items-center gap-3 text-left transition-colors hover:text-gray-50 ">
         <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
           <User className="h5 w5 text-gray-500" />
         </div>
