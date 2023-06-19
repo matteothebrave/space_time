ffb6f66fc964d9ca1747c6147a097f66854fc6c6
Matheus Grossi <thebravematteo33@protonmail.com>
2023-06-19 20:07:13 -0300

    feat/space-time/editing letters

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           <Image src={nlwLogo} alt="nlw space time" />
           <div className='max-w-[420px] space-y-1'>
             <h1 className='text-5xl font-bold leading-tight text-gray-50'>Sua cápsula do tempo</h1>
-            <p className='text-lg leading-relaxed'>Colecione momentos marcantes da sua jornada e compartilhe(se quiser) com o mundo</p>
+            <p className='text-lg leading-relaxed'>Colecione momentos incriveis da sua jornada e compartilhe(se quiser) com o mundo</p>
           </div>
 
           <a className="inline-block rounded-full bg-green-500 px-5 py-3 text-sm font-alt uppercase leading-none text-black hover:bg-green-600" href="">CADASTRAR LEMBRANÇA</a>
